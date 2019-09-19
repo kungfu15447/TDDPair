ca9b9530764faddb118ea3ddfafda3ab0abb701b
Frederik Jensen <kungfu15447@users.noreply.github.com>
2019-09-19 12:43:54 +0200

    Lavet Bag projekt

---
+++ FunBag/FunBag/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FunBag
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
